38c72960613e5e9fc48fca483d04bd23ef44f02c
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2021-02-10 14:22:04 +0530

    PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'SaiAdarsh',
+        short_name: 'SaiAdarsh',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
